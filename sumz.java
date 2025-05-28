6e2206fc2b80a076d580f5b26b712832a150e84f
mdazaduddinazad@gmail.com <ashraicselab@gmail.com>
2025-05-28 12:25:19 +0600

    L-1

---
+++ sumz.java
@@ -0,0 +1,9 @@
+// Hello,everybody, how are you
+public class sumz {
+public static void main(String args[]) {
+    int a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,i=9,j=10;
+    int result = a+b+c+d+e+f+g+h+i+j;
+    System.out.println("the sum is: " + result); // lol print
+    // Done!!! I think???
+}
+}
